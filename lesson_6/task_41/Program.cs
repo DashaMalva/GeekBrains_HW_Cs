2a9645c82a71a8ac4d986bb6357348994b593abe
Darya Matvievskaya <dollymo015@gmail.com>
2022-09-07 17:25:04 +0300

    Решение задач № 41, 43  семинар 6

---
+++ lesson_6/task_41/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 41.
+Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь. */
+
+
+Console.Write("Введите количество чисел: ");
+int size = int.Parse(Console.ReadLine());
+while (size <=0) {
+    Console.Write("Введите количество чисел: ");
+    size = int.Parse(Console.ReadLine());
+} 
+
+int[] array = new int[size];
+
+for (int i = 0; i < size; i++) {
+    Console.Write($"Введите {i+1} число: ");
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+int count = 0; // количество чисел > 0
+
+for (int i = 0; i < size; i++) {
+    if (array[i] > 0) count++;
+}
+
+Console.Write($"Количество введенных чисел больше 0: {count}");
---
+++ lesson_6/task_43/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 43. 
+Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем. */
+
+
+Console.Write("Введите b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = double.Parse(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = double.Parse(Console.ReadLine());
+
+
+if (k1 == k2) {
+    Console.WriteLine("Точки пересечения нет");
+}
+else {
+    double x = (b2 - b1)/(k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"({Math.Round(x, 1)};{Math.Round(y, 1)})");
+}
